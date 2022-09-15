706cff5e811aad3d0f4483560bdc39713579aecd
benardop <benard.oc.p@gmail.com>
2022-09-16 00:01:29 +0300

    Update app script file with new scripts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+// eslint-disable-next-line import/no-named-as-default
 import Calculator from './components/Calculator';
 
 class App extends React.PureComponent {
