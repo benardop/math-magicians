55e7f1f572fbf181f78714c54aae8822371f2917
benardop <benard.oc.p@gmail.com>
2022-09-16 14:30:37 +0300

    Update app.js script file and remove linter errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-// eslint-disable-next-line import/no-named-as-default
 import Calculator from './components/Calculator';
 
 class App extends React.PureComponent {
