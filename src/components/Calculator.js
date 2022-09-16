fd35feef43b72c593d7718db39391f83508eb48c
benardop <benard.oc.p@gmail.com>
2022-09-16 14:32:17 +0300

    Recfactor Calculator script class with reach hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Item from './Item';
+import ButtonItem from './ButtonItem';
 import Calculate from '../logic/calculate';
 
 function Calculator() {
@@ -27,25 +27,25 @@ function Calculator() {
   return (
     <div className="calculator">
       <div id="display" className="input-row">{display}</div>
-      <Item label="AC" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="+/-" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="%" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="÷" cl="orange" objetChangeEvent={objetChangeEvent} />
-      <Item label="7" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="8" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="9" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="x" cl="orange" objetChangeEvent={objetChangeEvent} />
-      <Item label="4" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="5" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="6" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="-" cl="orange" objetChangeEvent={objetChangeEvent} />
-      <Item label="1" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="2" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="3" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="+" cl="orange" objetChangeEvent={objetChangeEvent} />
-      <Item label="0" cl="double light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="." cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <Item label="=" cl="orange" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="AC" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="+/-" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="%" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="÷" cl="orange" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="7" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="8" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="9" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="x" cl="orange" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="4" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="5" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="6" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="-" cl="orange" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="1" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="2" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="3" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="+" cl="orange" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="0" cl="double light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="." cl="light-gray" objetChangeEvent={objetChangeEvent} />
+      <ButtonItem label="=" cl="orange" objetChangeEvent={objetChangeEvent} />
     </div>
   );
 }
