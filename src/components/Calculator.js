738c4805b22787f6dcd2c49d950ea77c6afc8cbc
benardop <benard.oc.p@gmail.com>
2022-09-22 16:17:28 +0300

    Update Calculator class with home page details

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React, { useState } from 'react';
 import ButtonItem from './ButtonItem';
 import calculate from '../logic/calculate';
@@ -15,27 +17,30 @@ function Calculator() {
   };
 
   return (
-    <div className="calculator">
-      <div id="display" className="input-row">{object.next || object.total || 0}</div>
-      <ButtonItem label="AC" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="+/-" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="%" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="÷" cl="orange" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="7" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="8" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="9" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="x" cl="orange" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="4" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="5" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="6" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="-" cl="orange" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="1" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="2" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="3" cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="+" cl="orange" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="0" cl="double light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="." cl="light-gray" objetChangeEvent={objetChangeEvent} />
-      <ButtonItem label="=" cl="orange" objetChangeEvent={objetChangeEvent} />
+    <div className="calculator-container">
+      <h2>Let's do some math!</h2>
+      <div className="calculator">
+        <div id="display" className="input-row">{object.next || object.total || 0}</div>
+        <ButtonItem label="AC" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="+/-" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="%" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="÷" cl="orange" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="7" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="8" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="9" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="x" cl="orange" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="4" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="5" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="6" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="-" cl="orange" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="1" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="2" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="3" cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="+" cl="orange" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="0" cl="double light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="." cl="light-gray" objetChangeEvent={objetChangeEvent} />
+        <ButtonItem label="=" cl="orange" objetChangeEvent={objetChangeEvent} />
+      </div>
     </div>
   );
 }
